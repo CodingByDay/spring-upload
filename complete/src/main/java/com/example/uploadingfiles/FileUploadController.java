1932a2635bc6d9eea1e53a6177ffc2d1eedf3dda
hanumansitaram <hanuman@unv.rs>
2021-09-24 11:33:41 +0200

    Working API.

--- complete/src/main/java/com/example/uploadingfiles/FileUploadController.java
+++ complete/src/main/java/com/example/uploadingfiles/FileUploadController.java
@@ -4,67 +4,60 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.stream.Collectors;
 import com.example.uploadingfiles.database.DatabaseUtils;
 import com.example.uploadingfiles.storage.StorageProperties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.apache.commons.io.FilenameUtils;
 import com.example.uploadingfiles.storage.StorageService;
 
-@Controller
+@RestController
 public class FileUploadController {
 
 	private final StorageService storageService;
+	private boolean available = true;
 
 	@Autowired
 	public FileUploadController(StorageService storageService) {
+
 		this.storageService = storageService;
 	}
 
 
 	// TODO: QUEUE IMPLEMENTATION.
+	// Is API available?
 
-
-	@GetMapping("/")
-	public String listUploadedFiles(Model model) throws IOException {
-
-		model.addAttribute("files", storageService.loadAll().map(
-				path -> MvcUriComponentsBuilder.fromMethodName(FileUploadController.class,
-						"serveFile", path.getFileName().toString()).build().toUri().toString())
-				.collect(Collectors.toList()));
-
-		return "uploadForm";
+	@GetMapping("/check")
+	public ResponseEntity<Boolean> checkAvailability() {
+		return new ResponseEntity<>(this.available, HttpStatus.OK);
 	}
 
-	@GetMapping("/files/{filename:.+}")
-	@ResponseBody
-	public ResponseEntity<Resource> serveFile(@PathVariable String filename) {
 
-		Resource file = storageService.loadAsResource(filename);
-		return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
-				"attachment; filename=\"" + file.getFilename() + "\"").body(file);
-	}
+
 
 	@PostMapping("/")
-	public String handleFileUpload(@RequestParam("file") MultipartFile file,
+	public ResponseEntity<String> handleFileUpload(@RequestParam("file") MultipartFile file,
 			RedirectAttributes redirectAttributes) throws IOException {
+		this.available = false;
 		// Grant permissions.
 		StorageProperties properties = new StorageProperties();
+
 		Path folderPermissions = Paths.get(properties.getLocation());
 		File owner = new File(String.valueOf(folderPermissions));
+
 		owner.setReadable(true, false);
 		owner.setWritable(true, false);
 		owner.setExecutable(true, false);
@@ -73,8 +66,6 @@ public class FileUploadController {
 		Path path = storageService.store(file);
 
 		// Now give the specific file all the above permissions.
-
-
 		File specific = new File(String.valueOf(path));
 
 		specific.setExecutable(true, false);
@@ -87,8 +78,8 @@ public class FileUploadController {
 				"You successfully uploaded " + file.getOriginalFilename() + "!");
 
 		RestoreDatabase(file.getOriginalFilename(), path);
-
-		return "redirect:/";
+		this.available = true;
+		return new ResponseEntity<>("Success", HttpStatus.OK);
 	}
 
 	private void RestoreDatabase(String fileName, Path path) {
