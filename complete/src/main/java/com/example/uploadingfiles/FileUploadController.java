0b61f8d7cc53ca3934ac28626c435250bd35a4f6
hanumansitaram <hanuman@unv.rs>
2021-09-24 10:49:55 +0200

    Working API

--- complete/src/main/java/com/example/uploadingfiles/FileUploadController.java
+++ complete/src/main/java/com/example/uploadingfiles/FileUploadController.java
@@ -2,15 +2,9 @@ package com.example.uploadingfiles;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.attribute.FileAttribute;
-import java.nio.file.attribute.PosixFilePermission;
-import java.nio.file.attribute.PosixFilePermissions;
-import java.util.Set;
 import java.util.stream.Collectors;
-
 import com.example.uploadingfiles.database.DatabaseUtils;
 import com.example.uploadingfiles.storage.StorageProperties;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,7 +13,6 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -29,7 +22,6 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.apache.commons.io.FilenameUtils;
-import com.example.uploadingfiles.storage.StorageFileNotFoundException;
 import com.example.uploadingfiles.storage.StorageService;
 
 @Controller
@@ -41,6 +33,10 @@ public class FileUploadController {
 	public FileUploadController(StorageService storageService) {
 		this.storageService = storageService;
 	}
+
+
+	// TODO: QUEUE IMPLEMENTATION.
+
 
 	@GetMapping("/")
 	public String listUploadedFiles(Model model) throws IOException {
@@ -68,17 +64,25 @@ public class FileUploadController {
 		// Grant permissions.
 		StorageProperties properties = new StorageProperties();
 		Path folderPermissions = Paths.get(properties.getLocation());
-
 		File owner = new File(String.valueOf(folderPermissions));
-
 		owner.setReadable(true, false);
-
 		owner.setWritable(true, false);
-
 		owner.setExecutable(true, false);
 
-
+		// Result
 		Path path = storageService.store(file);
+
+		// Now give the specific file all the above permissions.
+
+
+		File specific = new File(String.valueOf(path));
+
+		specific.setExecutable(true, false);
+		specific.setWritable(true, false);
+		specific.setReadable(true, false);
+
+		grantPermissions(String.valueOf(path));
+
 		redirectAttributes.addFlashAttribute("message",
 				"You successfully uploaded " + file.getOriginalFilename() + "!");
 
@@ -90,14 +94,9 @@ public class FileUploadController {
 	private void RestoreDatabase(String fileName, Path path) {
 
 		try {
-
-
 			File file = new File(String.valueOf(path));
-
 			String name = FilenameUtils.removeExtension(fileName);
-
 			DatabaseUtils.drop(name);
-
 			DatabaseUtils.restore(name, path);
 
 		} catch (Exception e) {
@@ -105,5 +104,14 @@ public class FileUploadController {
 		}
 	}
 
+
+
+	private void grantPermissions(String path) throws IOException {
+		ProcessBuilder builder = new ProcessBuilder(
+				"cmd.exe", "/c", "cd \"C:\\Windows\\System32\" && icacls "+"\"" + path + "\""+ " /grant \"Users\":F ");
+
+		builder.redirectErrorStream(true);
+		builder.start();
+	}
 
 }
--- complete/src/main/java/com/example/uploadingfiles/database/DatabaseUtils.java
+++ complete/src/main/java/com/example/uploadingfiles/database/DatabaseUtils.java
@@ -9,12 +9,8 @@ public class DatabaseUtils {
             throws IOException, InterruptedException {
 
         String out = String.format("sqlcmd -S localhost -U app -P app -Q \"DROP DATABASE [%s]\"", dbName);
-
-
         Process runtimeProcess = Runtime.getRuntime().exec(out);
-
         int processComplete = runtimeProcess.waitFor();
-
         return processComplete == 0;
     }
 
@@ -25,12 +21,8 @@ public class DatabaseUtils {
 
             String stringPath = String.valueOf(path);
             String out = String.format("sqlcmd -S localhost -U app -P app -Q \"RESTORE DATABASE [%s] FROM DISK = N'%s' \"", dbName, stringPath);
-
-
             Process runtimeProcess = Runtime.getRuntime().exec(out);
-
             int processComplete = runtimeProcess.waitFor();
-
             return processComplete == 0;
         }
     }
